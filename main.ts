2ad4ac85cfd4fe65400d4d320df95ceac947add0
euberdeveloper <euberdeveloper>
2020-03-29 16:25:20 +0200

    improvement(main.ts): starting adding middlewares

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-import * as express from 'express';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
 import * as morgan from 'morgan';
+import * as compression from 'compression';
 
-
+import * as express from 'express';
 const app = express();
+
+app.use(compression());
+

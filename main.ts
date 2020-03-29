66b3722cadbd50b58c8cfb1f5f4fea6e09774744
euberdeveloper <euberdeveloper>
2020-03-29 16:27:43 +0200

    improvement(main.ts): added middlewares

--- main.ts
+++ main.ts
@@ -2,9 +2,15 @@ import * as helmet from 'helmet';
 import * as cors from 'cors';
 import * as morgan from 'morgan';
 import * as compression from 'compression';
+import * as bodyParser from 'body-parser';
 
 import * as express from 'express';
 const app = express();
 
 app.use(compression());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 

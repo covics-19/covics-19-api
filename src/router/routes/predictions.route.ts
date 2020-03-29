6c1fb0aea188b31a0e7f63433f688fc894f7ff3d
euberdeveloper <euberdeveloper>
2020-03-30 00:16:55 +0200

    style: renamed route

--- src/router/routes/predictions.route.ts
+++ src/router/routes/predictions.route.ts
@@ -3,9 +3,11 @@ import { Database } from '../../utils/database';
 import { parsePredictions } from '../../utils/parsePredictions';
 
 export function addRoute(router: Router, database: Database): void {
+
     router.get('/predictions', async (_req, res) => {
         const predictions = await database.getLastPrediction();
         const parsedPredictions = parsePredictions(predictions);
         res.send(parsedPredictions);
     });
+    
 }

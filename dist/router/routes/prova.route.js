4db17569ebdc49f6ac529a2fe55dc24bb52f63e1
euberdeveloper <euberdeveloper>
2020-03-30 01:07:03 +0200

    build(dist): build for heroku

--- dist/router/routes/prova.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function addRoute(router) {
-    router.get('/prova', (req, res) => {
-        res.send('prova');
-    });
-}
-exports.addRoute = addRoute;

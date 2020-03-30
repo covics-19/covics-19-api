5277c6e36a4c765c1abe1b4d46becd3d443936f8
euberdeveloper <euberdeveloper@gmail.com>
2020-03-30 18:36:23 +0200

    build(dist): transpiled

--- dist/utils/parsePredictions.js
+++ dist/utils/parsePredictions.js
@@ -1,8 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const BIAS = 20;
 function parseCountryPrediction(prediction) {
-    const resources_prediction_3w = prediction.resources_capacity - (prediction.confirmed_prediction_3w - prediction.deaths_prediction_3w - prediction.recovered_prediction_3w) / 5;
-    return Object.assign(Object.assign({}, prediction), { resources_prediction_3w });
+    const resources_requirements = (prediction.confirmed - prediction.deaths - prediction.recovered) / BIAS;
+    const resources_requirements_prediction_3w = (prediction.confirmed_prediction_3w - prediction.deaths_prediction_3w - prediction.recovered_prediction_3w) / BIAS;
+    const available_resources = prediction.resources_capacity - (prediction.confirmed - prediction.deaths - prediction.recovered) / BIAS;
+    const available_resources_prediction_3w = prediction.resources_capacity - (prediction.confirmed_prediction_3w - prediction.deaths_prediction_3w - prediction.recovered_prediction_3w) / BIAS;
+    return Object.assign(Object.assign({}, prediction), { resources_requirements,
+        resources_requirements_prediction_3w,
+        available_resources,
+        available_resources_prediction_3w });
 }
 exports.parseCountryPrediction = parseCountryPrediction;
 function parsePredictions(predictons) {

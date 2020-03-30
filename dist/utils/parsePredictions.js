8f416b45601a279c18fa196c0ecf19dcf3e51743
euberdeveloper <euberdeveloper@gmail.com>
2020-03-30 15:57:40 +0200

    improvement: better parsing

--- dist/utils/parsePredictions.js
+++ dist/utils/parsePredictions.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function parseCountryPrediction(prediction) {
-    const resources_prediction_3w = prediction.confirmed_prediction_3w - prediction.deaths_prediction_3w - prediction.recovered_prediction_3w;
+    const resources_prediction_3w = prediction.resources_capacity - (prediction.confirmed_prediction_3w - prediction.deaths_prediction_3w - prediction.recovered_prediction_3w) / 20;
     return Object.assign(Object.assign({}, prediction), { resources_prediction_3w });
 }
 exports.parseCountryPrediction = parseCountryPrediction;

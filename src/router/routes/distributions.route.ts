4741b174abcc15df74beb0e2fc0d290e9d3898b9
euberdeveloper <euberdeveloper>
2020-03-30 13:31:17 +0200

    improvement: going on

--- src/router/routes/distributions.route.ts
+++ src/router/routes/distributions.route.ts
@@ -1,1 +1,13 @@
 
+import { Router } from 'express';
+import { Database } from '../../utils/database';
+
+export function addRoute(router: Router, database: Database): void {
+
+    router.get('/predictions', async (_req, res) => {
+        const predictions = await database.getLastPrediction();
+        const parsedPredictions = parsePredictions(predictions);
+        res.send(parsedPredictions);
+    });
+    
+}

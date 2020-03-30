43d228e580c45e859c75c097be97dd5b65bd1f3f
covics-team-19 <62818334+covics-team-19@users.noreply.github.com>
2020-03-30 13:13:18 +0200

    improvement: going on

--- src/options/index.ts
+++ src/options/index.ts
@@ -6,6 +6,6 @@ export default {
         uri: process.env.DB_URI || 'mongodb://localhost:27017',
         db: 'covics-19',
         collection_predictions: 'predictions',
-        collection_contributions: 'contributions'
+        collection_distributions: 'distributions'
     }
 };
